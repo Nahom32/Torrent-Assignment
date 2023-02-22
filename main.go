f318448471d4cc48c27fad0a8da8ad75ff6bac33
Paulos Dessie Nedaw <93246229+PaulosNed@users.noreply.github.com>
2023-02-22 20:47:17 +0300

    filtered the main file

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	// "crypto/sha1"
 	"fmt"
-	// "io/ioutil"
 	"os"
-	//"net"
 	"torrentassignment/generateTorrent"
 	"torrentassignment/leecher"
 	"torrentassignment/seeder"
-	//"log"
 
 )
 
@@ -32,7 +28,6 @@ func main() {
 	case "seed":
 		if (len(args) == 3){
 			fmt.Println("Seeding torrent:", args[2])
-			//log.Print(leecher.ReadTorrentFile(args[2]))
 			seeder.Seed(args[2])
 			}else{
 			fmt.Println("No")
@@ -50,54 +45,4 @@ func main() {
 	default:
 		fmt.Println("Unknown command:", command)
 	}
-
-	// // Open the file for reading
-	// file, err := os.Open("video.mkv")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer file.Close()
-
-	// // Determine the size of the file
-	// fileInfo, err := file.Stat()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fileSize := fileInfo.Size()
-
-	// // Define the chunk size
-	// chunkSize := int64(1024 * 1024) // 1 KB
-
-	// // Compute the number of chunks
-	// numChunks := fileSize / chunkSize
-	// if fileSize%chunkSize != 0 {
-	// 	numChunks++
-	// }
-
-	// // Read each chunk and compute the SHA1 hash
-	// hash := sha1.New()
-	// for i := int64(0); i < numChunks; i++ {
-	// 	// Read the next chunk
-	// 	offset := i * chunkSize
-	// 	if i == numChunks-1 {
-	// 		chunkSize = fileSize - offset
-	// 	}
-	// 	chunk := make([]byte, chunkSize)
-	// 	_, err := file.ReadAt(chunk, offset)
-	// 	if err != nil {
-	// 		panic(err)
-	// 	}
-
-	// 	// Compute the SHA1 hash of the chunk
-	// 	hash.Write(chunk)
-	// 	chunkHash := hash.Sum(nil)
-	// 	hash.Reset()
-
-	// 	// Print the hash of the chunk
-	// 	fmt.Printf("Chunk %d hash: %x\n", i+1, chunkHash)
-	// }
-
-	// // Print the final info hash
-	// finalHash := hash.Sum(nil)
-	// fmt.Printf("Final info hash: %x\n", finalHash)
 }
